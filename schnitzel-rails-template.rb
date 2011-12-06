48ec7b38fa414ff229d509865f9da9ed8cf88b00
Hendrik Mans <hendrik@mans.de>
2011-12-06 19:53:18 +0100

    Here goes the code.

---
+++ schnitzel-rails-template.rb
@@ -0,0 +1,18 @@
+initializer 'generators.rb', <<-EOD
+Rails.application.config.generators do |g|
+  g.orm             :active_record
+  g.template_engine :haml
+  g.test_framework  :rspec, :fixture => false
+  g.stylesheets     false
+  g.fixture_replacement :fabrication
+end
+EOD
+
+# add our favourite gems
+gem 'inherited_resources'
+gem 'haml-rails'
+
+gem 'rspec-rails', '>= 2.0.1', :group => [:development, :test]
+gem 'fabrication', :group => [:development, :test]
+gem 'ffaker', :group => [:development, :test]
+gem 'shoulda-matchers', :group => [:development, :test]
